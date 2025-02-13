1133df9989b1900fa6274490ca17dec361b9478d
cristhian-fernandez-cumbia <cfernandez@newip.pe>
2025-02-13 02:59:59 -0500

    feat: adaptar mobile

--- src/sections/Caracteristicas.tsx
+++ src/sections/Caracteristicas.tsx
@@ -30,7 +30,7 @@ const Caracteristicas = () => {
 
       <div className='w-full lg:w-1/3'>
         <div className='h-32 flex justify-center items-center relative'>
-          <span className='text-[18px] font-semibold px-20 text-center text-[#6BAEC3] '>eficiencia en el corazón de la minería</span>
+          <span className='text-[18px] font-semibold px-20 text-center text-[#6BAEC3] lg:text-lg xl:text-xl 2xl:text-2xl'>eficiencia en el corazón de la minería</span>
           <div className='absolute top-2 left-2 w-6 h-6 bg-orange-500'></div>
           <div className='absolute bottom-2 right-2 w-6 h-6 bg-blue-900'></div>
         </div>
